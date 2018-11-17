90a2e7b2a2bab64756bd9f209daf0cdcfe3c5cd1
Zekiie Irikh <zekie.irikh@gmail.com>
2018-11-17 22:06:53 +0200

    New task

---
+++ Technical_Page/gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp'), // Подключаем Gulp
+	sass = require('gulp-sass'), // Подключаем Sass пакет
+	autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', function() { // Создаем таск "sass"
+	return gulp.src(['sass/**/*.sass', 'sass/**/*.scss']) // Берем источник
+		.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError)) // Преобразуем Sass в CSS посредством gulp-sass
+		.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+		.pipe(gulp.dest('css')) // Выгружаем результата в папку css
+	});
+
+gulp.task('watch', function() {
+	gulp.watch(['sass/**/*.scss'], ['sass']); // Наблюдение за sass файлами в папке sass
+});
+
+gulp.task('default', ['watch']);
